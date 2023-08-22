438492b091f65d96521f07aecef41c0b60d25f09
Terry <master_correo144@hotmail.com>
2023-08-22 12:28:57 -0500

    FIXED vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ecommerce/",
+  base: "/tic-tac-toe/",
   plugins: [react()],
 })
